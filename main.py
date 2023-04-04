0f9badd1166ebc9917f4d1b8441a63375e561149
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-05 01:15:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,63 @@
 import requests
-import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 
-# Load Telegram API token from file
-with open("telegram_api_token.txt") as f:
-    telegram_api_token = f.read().strip()
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-# Create Telegram bot
-bot = telegram.Bot(token=telegram_api_token)
+class ShortenerAPIs:
+    URLEARN = 0
+    SHAREUS = 1
 
+class Shortner:
+    @staticmethod
+    def urlearn(query):
+        x = requests.get(f'https://urlearn.xyz/api?api=b3d7cfc604d7fd944e84533890593bc57960d7b5&url={query}').json()
+        if x.get('status') == 'success':
+            return x.get('shortenedUrl')
 
-# URL shortening functions
-def retrieve_original_url(short_url):
-    r = requests.get(short_url)
-    return r.url
+    @staticmethod
+    def shareus(query):
+        x = requests.get(f'https://api.shareus.in/shortLink?token=H8eTsJrrYFZu0BcH944aEkek93p2&link={query}', headers=headers).text
+        if x is not None:
+            return x
 
-
-# Telegram bot functions
 def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Hi! I can help you retrieve the original URL of a shortened link. Just send me the link and I'll do the rest.")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! I'm a link shortener bot. Send me a link and I'll shorten it for you.")
 
+def link(update, context):
+    text = update.message.text
+    try:
+        command, url = text.split(" ", 1)
+    except ValueError:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a link to shorten after the command.")
+        return
+    keyboard = [[InlineKeyboardButton("Urlearn", callback_data=str(ShortenerAPIs.URLEARN)),
+                 InlineKeyboardButton("Shareus", callback_data=str(ShortenerAPIs.SHAREUS))]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.user_data['url'] = url
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Which shortener do you want to use?", reply_markup=reply_markup)
 
-def shorten(update, context):
-    message = update.message
-    text = message.text
-    if text is not None and not (text.startswith("http") or text.startswith("www")):
-        url = message.text
-        original_url = retrieve_original_url(url)
-        context.bot.send_message(chat_id=message.chat_id, text=original_url)
-
+def button(update, context):
+    query = update.callback_query
+    api = int(query.data)
+    url = context.user_data['url']
+    if api == ShortenerAPIs.URLEARN:
+        shortened_url = Shortner.urlearn(url)
+    elif api == ShortenerAPIs.SHAREUS:
+        shortened_url = Shortner.shareus(url)
+    if shortened_url is not None:
+        query.edit_message_text(text=f"Here's your shortened link:\n\n{shortened_url}")
+    else:
+        query.edit_message_text(text="Sorry, an error occurred while shortening your link.")
 
 def main():
-    # Set up the Telegram updater and dispatcher
-    updater = Updater(token=telegram_api_token, use_context=True)
-    dispatcher = updater.dispatcher
-
-    # Add command handlers
-    dispatcher.add_handler(CommandHandler("start", start))
-
-    # Add message handlers
-    dispatcher.add_handler(MessageHandler(Filters.text, shorten))
-
-    # Start the bot
+    updater = Updater("5931504207:AAHNzBcYEEX7AD29L0TqWF28axqivgoaKUk", use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("link", link))
+    dp.add_handler(CallbackQueryHandler(button))
     updater.start_polling()
     updater.idle()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
